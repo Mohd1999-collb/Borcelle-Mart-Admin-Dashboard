7155b7177380c46c49c9ed4c744c1efe535d6065
Mohd199-collb <talibsidaug1999@gmail.com>
2024-07-11 12:58:31 +0530

    First Commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,89 @@
+import SalesChart from "@/components/custom ui/SalesChart";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import {
+  getSalesPerMonth,
+  getTotalCustomers,
+  getTotalSales,
+} from "@/lib/actions/actions";
+// import { useUser } from "@clerk/nextjs";
+import { BadgeIndianRupee, ShoppingBag, UserRound } from "lucide-react";
+// import { useRouter } from "next/router";
+// import { useRouter } from "next/navigation";
+
+
+
+
+export default async function Home() {
+
+  // const { user } = useUser();
+  // const router = useRouter();
+
+  const totalRevenue = await getTotalSales().then((data) => data.totalRevenue);
+  const totalOrders = await getTotalSales().then((data) => data.totalOrders);
+  const totalCustomers = await getTotalCustomers();
+
+  const graphData = await getSalesPerMonth();
+
+  // const handleLike = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  //   e.preventDefault();
+  //   try {
+  //     if (!user) {
+  //       router.push("/sign-in");
+  //       return;
+  //     } else {
+        
+  //     }
+  //   } catch (err) {
+  //     console.log("[wishlist_POST]", err);
+  //   }
+  // };
+
+  return (
+    <div className="px-8 py-10">
+      <p className="text-heading2-bold">Dashboard</p>
+      <Separator className="bg-grey-1 my-5" />
+
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center">
+            <CardTitle>Total Revenue</CardTitle>
+            <BadgeIndianRupee  className="max-sm:hidden" />
+          </CardHeader>
+          <CardContent>
+            <p className="text-body-bold">₹ {totalRevenue}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center">
+            <CardTitle>Total Orders</CardTitle>
+            <ShoppingBag className="max-sm:hidden" />
+          </CardHeader>
+          <CardContent>
+            <p className="text-body-bold">{totalOrders}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center">
+            <CardTitle>Total Customer</CardTitle>
+            <UserRound className="max-sm:hidden" />
+          </CardHeader>
+          <CardContent>
+            <p className="text-body-bold">{totalCustomers}</p>
+          </CardContent>
+        </Card>
+      </div>
+
+      <Card className="mt-10">
+        <CardHeader>
+          <CardTitle>Sales Chart (₹)</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SalesChart data={graphData} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
